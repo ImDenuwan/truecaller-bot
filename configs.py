5e013ea2c26309043939eed48b7f35d8aa94f3a7
Chamindu Denuwan <90548223+ImDenuwan@users.noreply.github.com>
2023-05-06 17:13:14 +0530

    Update and rename config.py to configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "")
-    
+    LOGCHID = int(getenv("LOGCHID", "-1000112234"))
+    API = getenv("API", "abcdefu67-8dgdg")
 cfg = Config()
